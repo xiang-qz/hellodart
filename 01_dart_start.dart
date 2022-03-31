6226076f9357a114b1eedb270ca59c5c75530df0
xiang-qz <2924371960@qq.com>
2022-03-31 08:49:19 +0000

    document

---
+++ 01_dart_start.dart
@@ -0,0 +1,19 @@
+void variables(){
+    var name = "voryager I";
+    var year = 1977;
+    var antennaDiameter = 3.7;
+    var flybyObjects = ['Jupter','Saturn','Uranus','Neptune'];
+    var image = {
+        'tags': ['Saturns'],
+        'url': 'path//to//Saturn.jpg'
+    };
+
+    print(
+        'name is $name, year is $year,antennaDiameter is $antennaDiameter,flybyObjects is $flybyObjects,image is $image'
+    );
+}
+void main(List<String> args){
+    //变量
+    variables();
+
+}
