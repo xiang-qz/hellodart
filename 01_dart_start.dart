b92c1b9b499262fd3a105e6e50162394937bb46c
xiang-qz <2924371960@qq.com>
2022-04-13 15:25:16 +0000

    新增：函数、注释、导入部分内容

--- 01_dart_start.dart
+++ 01_dart_start.dart
@@ -16,6 +16,7 @@ void variables(){
         'name is $name, year is $year,antennaDiameter is $antennaDiameter,flybyObjects is $flybyObjects,image is $image'
     );
 }
+//流程控制
 void control_flow_statements() {
   print('\n');
   print('#' * 40);
@@ -45,9 +46,71 @@ void control_flow_statements() {
     print(year);
   }
 }
+// 函数 
+int fibonacci(int n) {
+  if (n == 0 || n == 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
 
+bool is_hit(String line, String target) {
+  return line.contains(target);
+}
+
+void functions() {
+  print('\n');
+  print('#' * 40);
+  print('函数');
+  print('#' * 40);
+
+  var fib = fibonacci(8);
+  print('fib 8 is $fib');
+
+  var flybyObjects = [
+    'hello',
+    'abc',
+    '123',
+    'how do you do',
+    'how are you'
+  ];
+  flybyObjects.where((name) => name.contains('you')).forEach(print);
+
+  var hit_lines = flybyObjects.where((x) => is_hit(x, 'o'));
+  for (var one in hit_lines) {
+    print('one is $one');
+  }
+}
+// 注释
+void comments() {
+  print('\n');
+  print('#' * 40);
+  print('注释');
+  print('#' * 40);
+
+  /// 这是一个文档注释。
+  /// 文档注释用于为库、类以及类的成员添加注释。
+  /// 像 IDE 和 dartdoc 这样的工具可以专门处理文档注释。
+  /* 也可以像这样使用单斜杠和星号的注释方式 */
+  print('comments prints nothing.');
+}
+// 导入
+void imports() {
+  print('\n');
+  print('#' * 40);
+  print('导入');
+  print('#' * 40);
+
+  var mypi = pi;
+  print('pi is $mypi');
+
+  var x = pi / 4;
+  var y = sin(x);
+  print('sin(pi/4) is $y');
+}
 
 Future<void> main(List<String> args) async {
   variables();
   control_flow_statements();
+  functions();
+  comments();
+  imports();
 }
